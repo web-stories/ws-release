21ce625daec0d79b8a8693bbd4bcf71a2bddcd3e
Fagner <github@fagnermartins.com>
2014-11-29 15:43:12 -0200

    Create proper error message when 'configs.properties' not found

--- src/main/java/org/webstories/release/utils/ConfigsStreamReader.java
+++ src/main/java/org/webstories/release/utils/ConfigsStreamReader.java
@@ -5,14 +5,19 @@ import java.io.InputStream;
 
 public class ConfigsStreamReader {
 	public void read( InputStreamAction action ) throws InputStreamException {
-		InputStream in = getClass().getResourceAsStream( "/configs.properties" );
+		InputStream configsStream = getClass().getResourceAsStream( "/configs.properties" );
+		
+		if ( configsStream == null ) {
+			throw new InputStreamException( "File 'configs.properties' not found in the classpath" );
+		}
+		
 		try {
-			action.run( in );
+			action.run( configsStream );
 		} catch ( Exception e ) {
 			throw new InputStreamException( e );
 		} finally {
 			try {
-				in.close();
+				configsStream.close();
 			} catch ( IOException e ) {
 				throw new InputStreamException( e );
 			}
--- src/main/java/org/webstories/release/utils/InputStreamException.java
+++ src/main/java/org/webstories/release/utils/InputStreamException.java
@@ -2,6 +2,9 @@ package org.webstories.release.utils;
 
 public class InputStreamException extends Exception {
 	private static final long serialVersionUID = 1;
+	public InputStreamException( String msg ) {
+		super( msg );
+	}
 	public InputStreamException( Throwable cause ) {
 		super( cause );
 	}
